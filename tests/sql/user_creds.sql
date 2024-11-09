3502a69baff6c00640a302ae97be22fdd3d18a7d
vianmiranda <38566642+Vian-Miranda@users.noreply.github.com>
2024-11-09 03:26:37 -0500

    Test SQL scripts

---
+++ tests/sql/user_creds.sql
@@ -0,0 +1,15 @@
+-- CREATE DATABASE trains;
+
+-- USE trains; 
+
+-- CREATE TABLE customers (
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     username VARCHAR(50) NOT NULL UNIQUE,
+--     password VARCHAR(50) NOT NULL,
+--     first_name VARCHAR(50),
+--     last_name VARCHAR(50)
+-- );
+
+-- INSERT INTO customers (username, password, first_name, last_name)
+-- VALUES ('user1', 'password1', 'John', 'Doe'),
+--        ('user2', 'password2', 'Jane', 'Smith');
